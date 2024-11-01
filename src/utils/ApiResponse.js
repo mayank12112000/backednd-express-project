2db261be9cd2bdb42601aa29b32d5a08ff278cb5
Mayank  Keshari <mayank.keshari.93@gmail.com>
2024-11-01 13:11:23 +0530

    response and error utility

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse{
+    constructor(statusCode,data,message = "success"){
+        this.status = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode
+    }
+
+}
